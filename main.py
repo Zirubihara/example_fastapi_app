2d4654ee719519c9c9647c35d3c96bd7b310759c
Zirubihara <krystianw.e.t@gmail.com>
2024-10-29 22:07:27 +0100

    added flake and user model

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # main.py
 from fastapi import FastAPI
 
+from models.user_model import User
+
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "Witaj w FastAPI!"}
+
+@app.post("/users/")
+async def create_user(user: User):
+    return {"user_id": user.id, "name": user.name, "email": user.email}
---
+++ models/user_model.py
@@ -0,0 +1,7 @@
+# models.py
+from pydantic import BaseModel, EmailStr
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
