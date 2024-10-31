82430e6eceb2e814e03f86428276702992baecd2
Zirubihara <krystianw.e.t@gmail.com>
2024-10-31 21:43:27 +0100

    added email validator for gmail only

--- models/user_model.py
+++ models/user_model.py
@@ -1,10 +1,16 @@
 # models.py
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 class User(BaseModel):
     id: int
     name: str
     email: EmailStr
+
+    @field_validator('email')
+    def email_must_be_gmail(cls, v):
+        if not v.endswith('@gmail.com'):
+            raise ValueError('Email must be a Gmail address (ending with @gmail.com)')
+        return v
 
     class Config:
         title = "User Model"
