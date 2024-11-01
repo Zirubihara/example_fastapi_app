2c444784de5150f74445e2b99a65e9801c9255f3
Zirubihara <krystianw.e.t@gmail.com>
2024-11-01 19:03:20 +0100

    made more validators

--- models/odd_numbers_reponse_model.py
+++ models/odd_numbers_reponse_model.py
@@ -1,8 +1,21 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, conlist, field_validator
+from typing import Annotated, List
 
 class OddNumbersResponse(BaseModel):
-    odd_numbers: List[int]
+    odd_numbers: Annotated[List[int], conlist(int, min_items=1)]
+    
+    @field_validator('odd_numbers', mode='before')
+    def validate_odd_numbers(cls, v):
+        for number in v:
+            if number % 2 == 0:
+                raise ValueError('All numbers must be odd')
+        return v
+    
+    @field_validator('odd_numbers', mode='after')
+    def validate_sum_under_limit(cls, v):
+        if sum(v) > 100:
+            raise ValueError("Sum of numbers must not exceed 100")
+        return v
 
     class Config:
         title = "Odd Numbers Response Model"
