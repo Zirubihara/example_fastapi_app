88b59273d8ccd85bde4b710033ba1987a227905d
Zirubihara <krystianw.e.t@gmail.com>
2024-11-04 20:35:13 +0100

    added more metadata and middleware

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 from fastapi import FastAPI, HTTPException
-
+from fastapi.middleware.cors import CORSMiddleware  # Added CORS
 from models.odd_numbers_reponse_model import OddNumbersResponse
 from models.user_model import User
 from models.user_response_model import UserResponse
 
 app = FastAPI()
 
+# Added CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
+
+@app.get("/", summary="Root endpoint")
 async def read_root() -> dict:
-    return {"message": "Witaj w FastAPI!"}
+    """Returns a greeting in FastAPI."""
+    return {"message": "Welcome to FastAPI!"}
 
 
 @app.post("/users/", response_model=UserResponse, status_code=201)
 async def create_user(user: User) -> UserResponse:
-    # Przykładowa logika do sprawdzenia, czy użytkownik już istnieje
-    if user.id <= 0:  # Przykład: nieprawidłowy identyfikator
+    """Creates a new user."""
+    if user.id <= 0:  # Example: invalid identifier
         raise HTTPException(status_code=400, detail="Invalid user ID.")
-
-    # Możesz dodać więcej logiki do tworzenia użytkownika
-    # Na przykład, jeśli użytkownik już istnieje:
-    # if user_exists(user.id):
-    #     raise HTTPException(status_code=409, detail="User already exists.")
 
     return UserResponse(user_id=user.id, name=user.name, email=user.email)
 
 
 @app.get("/odd-numbers/", response_model=OddNumbersResponse, status_code=200)
 async def get_odd_numbers(start: int, end: int) -> OddNumbersResponse:
+    """Returns odd numbers in the specified range."""
     if start > end:
         raise HTTPException(
             status_code=400, detail="Start must be less than or equal to end."
