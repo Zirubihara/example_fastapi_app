ae801a336c324aeb9cb4b7d67f24b525dc510cec
Zirubihara <krystianw.e.t@gmail.com>
2024-12-15 23:58:56 +0100

    added docstrings to user response  schema

--- schemas/user_response_model.py
+++ schemas/user_response_model.py
@@ -2,6 +2,18 @@ from pydantic import BaseModel
 
 
 class UserResponse(BaseModel):
+    """
+    Response model for user creation.
+
+    This model is used to represent the response returned when a user is created
+    or retrieved from the system. It includes the user's ID, name, and email address.
+
+    Attributes:
+        user_id (int): The unique identifier for the user.
+        name (str): The name of the user.
+        email (str): The email address of the user.
+    """
+
     user_id: int
     name: str
     email: str
